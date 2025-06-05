a5d4623a063c2bf034998fce8187576fa4584c12
Adam M. <ajm2327@nau.edu>
2025-06-05 14:46:39 -0700

    created new requierments file handled by pip to avoid dependency conflicts

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,12 @@
 pip cache purge
 
+pip install protobuf>=6.30.0
+pip install grpcio grpcio-status
+
 pip install langchain-google-genai==2.1.2
 
 pip install langchain==0.3.24 langchain_community==0.3.22 \
   langgraph==0.3.21 langgraph-prebuilt==0.1.7 alpaca-py \
   "async-timeout==4.0.0" "fsspec==2024.10.0" \
   python-dotenv numpy pandas scikit-learn tensorflow-cpu joblib \
-  requests beautifulsoup4 pytz
+  requests beautifulsoup4 pytz sqlalchemy psycopg2-binary
